f4a05ca8ae2bc3c8f5ad09527cd0089d78f566b4
Gaurav Gogoi <gauravgogoigp@gmail.com>
2024-02-15 22:47:10 +0530

    static work complete

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,92 @@ import { useState } from 'react'
   },
 ];
 
+function FriendsList(){
+  const friends = initialFriends
+
+
+  return <ul>
+
+    {friends.map((friend)=>{
+
+      return <Friend friend = {friend} key={friend.id}/>
+    })
+    }
+
+  </ul>
+
+}
+
+function Friend({friend}){
+
+  return <li>
+
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+    {friend.balance < 0 && <p className='red'>You owe {friend.name} ${Math.abs(friend.balance)} </p>}
+    {friend.balance > 0 && <p className='green'>{friend.name} owes you ${Math.abs(friend.balance)} </p>}
+    {friend.balance === 0 && <p>You and  {friend.name} are even </p>}
+    
+    <Button>Select</Button>
+  </li>
+
+}
+
+function Button({children}){
+
+return <button className="button">{children} </button>
+}
+
+function FormAddFriend(){
+
+    return <form className='form-add-friend'>
+      <label>🙆‍♂️Friend name</label>
+      <input type="text" />
+
+      <label>⛰️Image URL</label>
+      <input type="text" />
+
+      <Button>Add Friend</Button>
+    </form>
+
+}
+
+function FormSplitBill(){
+  return <form className="form-split-bill">
+
+    <h2>Split a bill with X</h2>
+    <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🤦‍♂️ Your Expense</label>
+      <input type="text" />
+
+      <label>🤦‍♀️ X's Expense</label>
+      <input type="text" disabled/>
+
+    <label > 🤷‍♂️ Who is playing the bill?</label>
+
+    <select>
+      <option value="user">You</option>
+      <option value="friend">X</option>
+    </select>
+    <Button>Add Friend</Button>
+  </form>
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
 
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      <div className="sidebar">
+          <FriendsList />
+          <FormAddFriend />
+          <Button>Add Friend</Button>
+      </div>
+      <FormSplitBill />
+    </div>
   )
 }
 
